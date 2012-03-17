4fea178ea7aab313199c954b92a8257c55caa49f
Alexander Surma <surma@surmair.de>
2012-03-17 17:40:58 +0100

    Implement Keyer

--- screenkey/ScreenKey.java
+++ screenkey/ScreenKey.java
@@ -5,13 +5,22 @@ import javax.imageio.*;
 
 public class ScreenKey {
 	public static void main(String[] args) {
+		Keyer k = new Keyer();
+		k.setKey(Color.WHITE);
+		k.setMaxDistance(40.0f);
 		try {
 			Robot r = new Robot();
-			BufferedImage bi = r.createScreenCapture(new Rectangle(0,0, 200, 200));
-			File f = new File("test.png");
-			ImageIO.write(bi, "png", f);
+			BufferedImage base = r.createScreenCapture(new Rectangle(0,0, 200, 200));
+			BufferedImage layer = r.createScreenCapture(new Rectangle(200,200, 200, 200));
+			BufferedImage mask = k.generateMask(layer);
+			BufferedImage result = k.key(layer, mask, base);
+			File f = new File("mask.png");
+			ImageIO.write(mask, "png", f);
+			f = new File("result.png");
+			ImageIO.write(result, "png", f);
 		} catch (Exception e) {
 			System.out.println("Ficken: "+e.toString());
+			e.printStackTrace();
 		}
 	}
 }
